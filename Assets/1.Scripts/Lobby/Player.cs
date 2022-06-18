73805cfb2d3d0534c9f9a009471f89e157fdc0a2
Kang Yechan <ghn05188@naver.com>
2022-06-19 00:54:33 +0900

    펫 소환 이펙트 추가, 랜덤하게 펫 등급과 이펙트 종류 결정

--- Assets/1.Scripts/Lobby/Player.cs
+++ Assets/1.Scripts/Lobby/Player.cs
@@ -9,11 +9,11 @@ public class Player : MonoBehaviour
     int curEventTypr = 0;
     string nextMoveScenes = "";
     string openUIName = "";
+
     // Start is called before the first frame update
     void Start()
     {
         //animConChanger = GetComponent<AnimConChanger>();
-
     }
 
 
