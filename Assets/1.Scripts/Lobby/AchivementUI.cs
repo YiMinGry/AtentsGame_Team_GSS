f8cda3098fbbc4c7102dbae759ed40ed2fcee8fd
ChangHyoen <dpdlfhf@gmail.com>
2022-06-20 09:41:12 +0900

    업적창 스크립트 약간수정

--- Assets/1.Scripts/Lobby/AchivementUI.cs
+++ Assets/1.Scripts/Lobby/AchivementUI.cs
@@ -7,6 +7,7 @@ public class AchivementUI : MonoBehaviour
     public GameObject contentsCard = null;
     public GameObject iconBar = null;
     private GameObject[] cards = new GameObject[100];
+    private GameObject[] icons = new GameObject[10];
 
     private void Awake()
     {
@@ -15,69 +16,54 @@ public class AchivementUI : MonoBehaviour
             cards[i] = contentsCard.transform.GetChild(i).gameObject;
 
         }
+        for(int i=0; i<iconBar.transform.childCount;i++)
+        {
+            icons[i] = iconBar.transform.GetChild(i).gameObject;
+
+        }
 
         OnClickButtonMain();
     }
-    public void OnClickButtonMain()
+
+    private void OnClickButton(string tagName)
     {
-        
-        for (int i=0;i<contentsCard.transform.childCount;i++)
+        for (int i = 0; i < contentsCard.transform.childCount; i++)
         {
-            if (!(cards[i].CompareTag("card_main")))
+            if (!(cards[i].CompareTag(tagName)))
                 cards[i].SetActive(false);
             else
                 cards[i].SetActive(true);
         }
+        for (int i = 0; i < iconBar.transform.childCount; i++)
+        {
+            if ((icons[i].CompareTag(tagName)))
+                icons[i].transform.localScale = new Vector3(1.3f, 1.3f, 1);
+            else
+                icons[i].transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+    public void OnClickButtonMain()
+    {
+        OnClickButton("card_main");
     }
     public void OnClickButtonFish()
     {
-        for (int i = 0; i < contentsCard.transform.childCount; i++)
-        {
-            if (!(cards[i].CompareTag("card_fish")))
-                cards[i].SetActive(false);
-            else
-                cards[i].SetActive(true);
-        }
+        OnClickButton("card_fish");
     }
     public void OnClickButtonCookie()
     {
-        for (int i = 0; i < contentsCard.transform.childCount; i++)
-        {
-            if (!(cards[i].CompareTag("card_cookie")))
-                cards[i].SetActive(false);
-            else
-                cards[i].SetActive(true);
-           
-        }
+        OnClickButton("card_cookie");
     }
     public void OnClickButtonWar()
     {
-        for (int i = 0; i < contentsCard.transform.childCount; i++)
-        {
-            if (!(cards[i].CompareTag("card_war")))
-                cards[i].SetActive(false);
-            else
-                cards[i].SetActive(true);
-        }
+        OnClickButton("card_war");
     }
     public void OnClickButtonRhythm()
     {
-        for (int i = 0; i < contentsCard.transform.childCount; i++)
-        {
-            if (!(cards[i].CompareTag("card_rhythm")))
-                cards[i].SetActive(false);
-            else
-                cards[i].SetActive(true);
-        }
+        OnClickButton("card_rhythm");
     }
     public void OnClickButtonBeer()
     {
-        for (int i = 0; i < contentsCard.transform.childCount; i++)
-        {
-            if (!(cards[i].CompareTag("card_beer")))
-                cards[i].SetActive(false);
-            else
-                cards[i].SetActive(true);
-        }
+        OnClickButton("card_beer");
     }
 }
