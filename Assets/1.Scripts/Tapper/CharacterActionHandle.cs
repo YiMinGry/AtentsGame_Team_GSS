b86b09f8930950687b858413059c6749af54b967
YiMinGyu <ymg9512@gmail.com>
2022-06-22 23:26:37 +0900

    태퍼 추가

---
+++ Assets/1.Scripts/Tapper/CharacterActionHandle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterActionHandle : MonoBehaviour
+{
+    public bool _isAttack = false;
+    public bool _isRun = false;
+
+    public void SetAttack(bool _TF) 
+    {
+        _isAttack = _TF;
+    }
+    public void SetRun(bool _TF)
+    {
+        _isRun = _TF;
+    }
+}
