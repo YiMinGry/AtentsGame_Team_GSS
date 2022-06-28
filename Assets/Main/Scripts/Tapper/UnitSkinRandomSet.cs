5da1c82c4e656593a859e50ca36863426b75c44d
YiMinGyu <ymg9512@gmail.com>
2022-06-29 06:55:44 +0900

    에디터 기능 전처리

--- Assets/Main/Scripts/Tapper/UnitSkinRandomSet.cs
+++ Assets/Main/Scripts/Tapper/UnitSkinRandomSet.cs
@@ -1,16 +1,19 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(UnitSkinManager))] 
-public class UnitSkinRandomSet : Editor 
-{ 
-    public override void OnInspectorGUI() 
+[CustomEditor(typeof(UnitSkinManager))]
+public class UnitSkinRandomSet : Editor
+{
+
+    public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
-        UnitSkinManager generator = (UnitSkinManager)target; 
-        if (GUILayout.Button("Random Set")) 
-        { 
-            generator.RandomSet(); 
-        } 
-    } 
+        UnitSkinManager generator = (UnitSkinManager)target;
+        if (GUILayout.Button("Random Set"))
+        {
+            generator.RandomSet();
+        }
+    }
 }
+#endif
